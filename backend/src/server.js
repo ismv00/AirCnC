dc18103d1053e3fb280dd866088660b0d98d0352
Igor Menezes <ismv00@icloud.com>
2020-11-22 23:40:12 -0400

    Front-end da aplicacao

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -10,8 +12,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@semanaoministack09.cnafk.mon
     useUnifiedTopology: true
 })
 
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
